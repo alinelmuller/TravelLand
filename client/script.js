c96ce60c4bafb1fc83f6013f6d75a548b7236b84
alinelmuller <alinelmuller@gmail.com>
2024-10-18 11:48:34 -0600

    structure

---
+++ client/script.js
@@ -0,0 +1,15 @@
+document.getElementById('add-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    // Add logic here
+    alert("Item Added!");
+});
+
+document.getElementById('edit-button').addEventListener('click', function(event) {
+    // Edit logic here
+    alert("Item Edited!");
+});
+
+document.getElementById('delete-button').addEventListener('click', function(event) {
+    // Delete logic here
+    alert("Item Deleted!");
+});
